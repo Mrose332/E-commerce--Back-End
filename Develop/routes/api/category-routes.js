d04536c0c12551c70c26dd7e1dcc2ef687804f02
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-03-05 14:24:19 -0500

    Update category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -16,26 +16,74 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'Category location not found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
+
+router.post('/', async (req, res) => {
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.put('/:id', async (req, res) => { 
+try {
+  const catData = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  if (!catData[0]) {
+    res.status(404).json({message: 'Category location not found with this id!'});
+  
+  } else 
+  
+  {
+    res.status(200).json(d);
+  }
+
+} catch (error) {
+  res.status(500).json(error);
+
+}
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const catData = await Location.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!catData) {
+      res.status(404).json({ message:  'Category location not found with this id!' });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
 
 module.exports = router;
