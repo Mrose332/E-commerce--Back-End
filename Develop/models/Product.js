a2c065f4d97cedfac7c4f952471d58d926d6561c
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-03-05 13:51:40 -0500

    commit changes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,8 +9,45 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true, 
+        notNull: true
+      }
+    }, 
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        notNull: true, 
+        isNumeric: true
+      }
+    },
+    category_id: { // References the Category model's ID. This product belongs to one category.
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category', 
+        key: 'id'
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
